73b7c35e90e361e5180f4a5b1ed5c955cf76404a
LEE026 <lhs456852@gmail.com>
2024-09-04 10:23:59 +0900

    spring 초기세팅

---
+++ src/main/java/com/oops/OvertureOfPromachina/OvertureOfPromachinaApplication.java
@@ -0,0 +1,13 @@
+package com.oops.OvertureOfPromachina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvertureOfPromachinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OvertureOfPromachinaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oops/OvertureOfPromachina/OvertureOfPromachinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oops.OvertureOfPromachina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OvertureOfPromachinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
