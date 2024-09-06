442a4eba8522ed0241fefbdb1bc950a4177b6be1
LEE026 <lhs456852@gmail.com>
2024-09-06 12:47:41 +0900

    기능: 게임 룸 엔티티 제작

---
+++ src/main/java/com/oops/OvertureOfPromachina/application/entity/GameRoom/GameRoom.java
@@ -0,0 +1,24 @@
+package com.oops.OvertureOfPromachina.application.entity.GameRoom;
+
+import com.oops.OvertureOfPromachina.application.entity.GameRoom.valueObject.GameMode;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class GameRoom {
+    @Id
+    @GeneratedValue
+    @Column(name = "game_room_id")
+    private Long id;
+
+    @Embedded
+    private GameMode gameMode;
+
+    public GameRoom(GameMode gameMode) {
+        this.gameMode = gameMode;
+    }
+}
