f2b6e5c866cd87651b2937740619e3456dbb1045
LEE026 <lhs456852@gmail.com>
2024-09-09 16:38:19 +0900

    기능: 게임룸에 제작 시간 저장

--- src/main/java/com/oops/OvertureOfPromachina/application/entity/GameRoom/GameRoom.java
+++ src/main/java/com/oops/OvertureOfPromachina/application/entity/GameRoom/GameRoom.java
@@ -6,10 +6,15 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class GameRoom {
     @Id
     @GeneratedValue
@@ -18,6 +23,10 @@ public class GameRoom {
 
     @Embedded
     private GameMode gameMode;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createTime;
 
     public GameRoom(GameModeEnum gameMode) {
         this.gameMode = new GameMode(gameMode);
