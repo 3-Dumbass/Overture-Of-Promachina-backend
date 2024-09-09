b31d8f2eb177eee424fddc892b7c3e6c41a3b403
LEE026 <lhs456852@gmail.com>
2024-09-09 16:40:18 +0900

    기능: 사용자의 방참가 시간 기록

--- src/main/java/com/oops/OvertureOfPromachina/application/entity/gameRoomParticipant/GameRoomParticipant.java
+++ src/main/java/com/oops/OvertureOfPromachina/application/entity/gameRoomParticipant/GameRoomParticipant.java
@@ -6,10 +6,15 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class GameRoomParticipant {
     @Id
     @GeneratedValue
@@ -22,6 +27,10 @@ public class GameRoomParticipant {
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime participateTime;
 
     public GameRoomParticipant(GameRoom gameRoom, User user) {
         this.gameRoom = gameRoom;
