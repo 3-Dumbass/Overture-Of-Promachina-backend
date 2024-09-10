c61688afbcd2e7bdb13a00cf09028a2cb237d1bd
LEE026 <lhs456852@gmail.com>
2024-09-10 15:12:50 +0900

    수정: 매핑방식 변경

--- src/main/java/com/oops/OvertureOfPromachina/application/controller/gameRoom/GameRoomController.java
+++ src/main/java/com/oops/OvertureOfPromachina/application/controller/gameRoom/GameRoomController.java
@@ -7,10 +7,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "game room api", description = "게임 방의 API 목록")
 @RestController
@@ -21,7 +18,7 @@ public class GameRoomController {
 
 
     @Operation(summary = "게임룸 제작", description = "유저가 게임룸을 제작한 후 입장합니다")
-    @GetMapping("/create")
+    @PostMapping("/create")
     public Long createGameRoom(@RequestBody @Valid GameRoomCreateRequest gameRoomCreateRequest){
         User user = null;
         return gameRoomService.makeRoom(user, gameRoomCreateRequest.getGameMode());
