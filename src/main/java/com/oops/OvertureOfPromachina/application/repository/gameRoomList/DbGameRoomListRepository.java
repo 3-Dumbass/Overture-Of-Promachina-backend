1fe6b821960a81022f4601a8c2861efc2566ef14
LEE026 <lhs456852@gmail.com>
2024-09-10 12:25:49 +0900

    기능: 게임룸 목록 레파지토리 구현체 제작

---
+++ src/main/java/com/oops/OvertureOfPromachina/application/repository/gameRoomList/DbGameRoomListRepository.java
@@ -0,0 +1,39 @@
+package com.oops.OvertureOfPromachina.application.repository.gameRoomList;
+
+import com.oops.OvertureOfPromachina.application.entity.GameRoom.GameRoom;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import static com.oops.OvertureOfPromachina.application.entity.GameRoom.QGameRoom.gameRoom;
+
+@Repository
+public class DbGameRoomListRepository implements GameRoomListRepository{
+
+    private final EntityManager em;
+    private final JPAQueryFactory query;
+
+    public DbGameRoomListRepository(EntityManager em) {
+        this.em = em;
+        this.query = new JPAQueryFactory(em);
+    }
+
+    @Override
+    public List<GameRoom> getGameRoomList(Long page, Long size) {
+        return query.select(gameRoom)
+                .from(gameRoom)
+                .orderBy(gameRoom.createTime.desc(), gameRoom.id.desc())
+                .offset(page * size)
+                .limit(size)
+                .fetch();
+    }
+
+    @Override
+    public Long getAllGameRoomAmount() {
+        return query.select(gameRoom.count())
+                .from(gameRoom)
+                .fetchOne();
+    }
+}
