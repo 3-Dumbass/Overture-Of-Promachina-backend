f827f48ed8e4f2e35662b6f39a30c6feed5cb45f
LEE026 <lhs456852@gmail.com>
2024-09-13 18:29:42 +0900

    테스트: 유저 레파지토리 테스트 작성

---
+++ src/test/java/com/oops/OvertureOfPromachina/application/repository/user/UserRepositoryTest.java
@@ -0,0 +1,26 @@
+package com.oops.OvertureOfPromachina.application.repository.user;
+
+import com.oops.OvertureOfPromachina.application.entity.user.User;
+import com.oops.OvertureOfPromachina.application.repository.User.UserRepository;
+import com.oops.OvertureOfPromachina.fixture.UserFixture;
+import com.oops.OvertureOfPromachina.testSetting.SpringTestSetting;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UserRepositoryTest extends SpringTestSetting {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void duplicateCheckUserTest(){
+        User user = UserFixture.create();
+        String loginId = user.getLoginId().getLoginId();
+        userRepository.save(user);
+
+        String result = userRepository.DuplicateCheckUser(loginId);
+
+        Assertions.assertThat(result).isEqualTo(loginId);
+    }
+}
