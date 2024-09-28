0c9157812cc2eacdfb72c2e51df62e5596d6797c
LEE026 <lhs456852@gmail.com>
2024-09-28 17:05:44 +0900

    기능: 현재당계에서 작업을 완료한 유저를 저장하는 변수 제작

--- src/main/java/com/oops/OvertureOfPromachina/common/gamePlayHandler/object/Game.java
+++ src/main/java/com/oops/OvertureOfPromachina/common/gamePlayHandler/object/Game.java
@@ -3,17 +3,22 @@ package com.oops.OvertureOfPromachina.common.gamePlayHandler.object;
 import com.oops.OvertureOfPromachina.common.gamePlayHandler.object.type.GameStep;
 import lombok.Getter;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
 
 @Getter
 public class Game {
     private final Long roomId;
     private final List<Long> userIdList;
+    private Set<Long> finishUsers;
     private GameStep gameStep;
 
     public Game(Long roomId, List<Long> userIdList) {
         this.roomId = roomId;
         this.userIdList = userIdList;
-        gameStep = GameStep.BETTING;
+        this.finishUsers = new ConcurrentSkipListSet<>();
+        this.gameStep = GameStep.BETTING;
     }
 }
