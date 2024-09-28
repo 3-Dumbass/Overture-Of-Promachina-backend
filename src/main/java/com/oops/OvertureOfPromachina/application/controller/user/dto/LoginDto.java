8055c52e3df7f78497e47b003809a012abd3a1d9
LEE026 <lhs456852@gmail.com>
2024-09-28 16:25:36 +0900

    핫픽스: LoginDto 생성자 추가

--- src/main/java/com/oops/OvertureOfPromachina/application/controller/user/dto/LoginDto.java
+++ src/main/java/com/oops/OvertureOfPromachina/application/controller/user/dto/LoginDto.java
@@ -1,11 +1,13 @@
 package com.oops.OvertureOfPromachina.application.controller.user.dto;
 
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class LoginDto {
 
     private String login_id;
