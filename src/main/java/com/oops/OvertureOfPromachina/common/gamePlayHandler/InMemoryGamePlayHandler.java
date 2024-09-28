44998dc99feaa2a90d98d058cb555ff3b45e618b
LEE026 <lhs456852@gmail.com>
2024-09-28 16:49:08 +0900

    기능: GamePlayHandler의 구현체 제작

---
+++ src/main/java/com/oops/OvertureOfPromachina/common/gamePlayHandler/InMemoryGamePlayHandler.java
@@ -0,0 +1,20 @@
+package com.oops.OvertureOfPromachina.common.gamePlayHandler;
+
+import com.oops.OvertureOfPromachina.common.gamePlayHandler.dto.GameStartDto;
+import com.oops.OvertureOfPromachina.common.gamePlayHandler.object.Game;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+public class InMemoryGamePlayHandler implements GamePlayHandler{
+
+    Map<Long, Game> gameList = new ConcurrentHashMap<>();
+
+    @Override
+    public void startGame(GameStartDto gameStartDto) {
+        Game game = new Game(gameStartDto.getRoomId(), gameStartDto.getUserIds());
+        gameList.put(game.getRoomId(), game);
+    }
+}
