c07d2a3239c0c51cde63263a6af875c69e79a085
heavenring <anstn0104@naver.com>
2024-10-01 01:08:01 +0900

    trade controller 제작

---
+++ src/main/java/com/oops/OvertureOfPromachina/application/controller/casinoChip/CasinoChipController.java
@@ -0,0 +1,34 @@
+package com.oops.OvertureOfPromachina.application.controller.casinoChip;
+
+
+import com.oops.OvertureOfPromachina.application.controller.casinoChip.dto.ChipDto;
+import com.oops.OvertureOfPromachina.application.entity.casinoChip.CasinoChip;
+import com.oops.OvertureOfPromachina.application.service.business.CasinoChip.CasinoChipService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "Chip api", description = "칩 교환 api")
+@RestController
+@RequestMapping("/api/chip")
+@RequiredArgsConstructor
+public class CasinoChipController {
+
+    private final CasinoChipService casinoChipService;
+
+    @Operation(summary = "chip trade api", description = "칩 보유 머니 업데이트 api")
+    @PostMapping("/trade")
+    public ResponseEntity<Boolean> chipTrade(@RequestBody @Valid ChipDto chipDto) {
+
+        CasinoChip casinoChip_data = casinoChipService.chip_trade(chipDto);
+
+        return ResponseEntity.ok()
+                .body(casinoChip_data.getMoney() == chipDto.getMoney());
+    }
+}
--- src/main/java/com/oops/OvertureOfPromachina/application/controller/user/LoginController.java
+++ src/main/java/com/oops/OvertureOfPromachina/application/controller/user/LoginController.java
@@ -6,9 +6,11 @@ import com.oops.OvertureOfPromachina.application.entity.user.User;
 import com.oops.OvertureOfPromachina.application.service.business.User.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +25,7 @@ public class LoginController {
 
     @Operation(summary = "login", description = "로그인")
     @PostMapping("/login")
-    public ResponseEntity<UserDto> login(LoginDto loginData) {
+    public ResponseEntity<UserDto> login(@RequestBody @Valid LoginDto loginData) {
 
         // login 시 있는 유저인지 확인
         User user_data = userService.loginUserData(loginData);
